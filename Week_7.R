5cf99ddee448d8b643b82b136ab3e95318cef007
nguyver <vuthynguy@gmail.com>
2015-03-22 23:42:24 -0400

    updates

--- Week_7.R
+++ Week_7.R
@@ -5,8 +5,8 @@
 
 #1)Write down 3 questions that you might want to answer based on this data.
 #a. Who votes more, 16-24 or 25+\
-#b. Does age group determine preferences
-#c. Does location determine preferences
+#b. Who votes more, Edinburgh, or Glasgow
+#c. Does age determine preferences
 
 install.packages("tidyr")
 install.packages("dplyr")
@@ -41,7 +41,37 @@ tidy$location[tidy$location == 'G'] <- 'Glasgow'
 #4) Use the functionality in dplyr package to answer the 
 #questions that you asked in step 1
 
-filer(tidy, Age)
+filter(tidy, Age=="16-24")
+filter(tidy, Age=="25+")
+
+#a) 16-24: 258400
+# 25+: 715200
+#25+ Votes more
+
+#b)
+filter(tidy, location=="Edinburgh")
+#473800
+filter(tidy, location=="Glasgow")
+#499800
+#Edinburgh votes more
+
+#c)
+filter(tidy, Vote=="Yes" & Age=="16-24")
+#179500
+filter(tidy, Vote=="Yes" & Age=="25+")
+#293400
+filter(tidy, Vote=="No" & Age=="16-24")
+#78900
+filter(tidy, Vote=="No" & Age=="25+")
+#421800
+
+#16-24 seem to prefer 'Yes
+#25+ Seems to prefer 'No'
+
 
 #5)Having gone through the process, would you ask different 
 #questions and/or change the way that you structured your data frame?
+
+#I had to change the structure of the data frame a few times 
+#just to be able to tidy it. I wouldn't change it now but I 
+# definately had to change it a few times before I was able to tidy it.
